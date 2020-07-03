cf7ddde67d81b9b203c55ce3b239bd41688657c4
polunwu <polunwu@gmail.com>
2020-07-03 20:33:17 +0800

    Testing emitted event via Vue Test Utils’ emmited API

---
+++ tests/unit/LoginForm.spec.js
@@ -0,0 +1,31 @@
+import LoginForm from '@/components/LoginForm.vue'
+import { mount } from '@vue/test-utils'
+
+describe('LoginForm', () => {
+  it('emits an event with a user data payload', () => {
+    const wrapper = mount(LoginForm) 
+
+    // 1. Find text input
+    const input = wrapper.find('[data-testid="name-input"]')
+
+    // 2. Set value for text input
+    input.setValue('Adam Jahr')
+
+    // 3. Simulate form submission
+    wrapper.trigger('submit')
+
+    // 4. Assert event has been emitted
+    // * wrapper.emitted() will return object
+    // { 
+    //    formSubmitted: [[{ name: 'Adam Jahr' }]]
+    //  }
+    const formSubmittedCalls = wrapper.emitted('formSubmitted')
+    expect(formSubmittedCalls).toHaveLength(1)
+
+    // 5. Assert payload is correct
+    const expectedPayload = {
+      name: 'Adam Jahr'
+    }
+    expect(wrapper.emitted('formSubmitted')[0][0]).toMatchObject(expectedPayload)
+  })
+})
